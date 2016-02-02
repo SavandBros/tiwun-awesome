e827dc63eed92c3d9d2f44b0018b3b929d63be00
amirm3hdi <amirm3hdi@gmail.com>
2016-02-02 16:17:08 +0400

    Add ui-router

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -40,6 +40,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/matchHeight/jquery.matchHeight-min.js',
+      'bower_components/angular-ui-router-title/angular-ui-router-title.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
