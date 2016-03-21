59e6889dd5fe3f87012030fe6d39d23c9bbfabf0
Alireza Savand <alireza.savand@gmail.com>
2016-03-22 03:30:04 +0400

    Using serveStatic now

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,7 @@ module.exports = function (grunt) {
         cdnify: 'grunt-google-cdn'
     });
     var modRewrite = require('connect-modrewrite');
+    var serveStatic = require('serve-static');
 
     grunt.loadNpmTasks('grunt-ng-constant');
 
@@ -88,16 +89,16 @@ module.exports = function (grunt) {
                     middleware: function (connect) {
                         return [
                             modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.woff|\\.ttf$ /index.html [L]']),
-                            connect.static('.tmp'),
+                            serveStatic('.tmp'),
                             connect().use(
                                 '/bower_components',
-                                connect.static('./bower_components')
+                                serveStatic('./bower_components')
                             ),
                             connect().use(
                                 '/app/styles',
-                                connect.static('./app/styles')
+                                serveStatic('./app/styles')
                             ),
-                            connect.static(appConfig.app)
+                            serveStatic(appConfig.app)
                         ];
                     }
                 }
@@ -107,13 +108,13 @@ module.exports = function (grunt) {
                     port: 9001,
                     middleware: function (connect) {
                         return [
-                            connect.static('.tmp'),
-                            connect.static('test'),
+                            serveStatic('.tmp'),
+                            serveStatic('test'),
                             connect().use(
                                 '/bower_components',
-                                connect.static('./bower_components')
+                                serveStatic('./bower_components')
                             ),
-                            connect.static(appConfig.app)
+                            serveStatic(appConfig.app)
                         ];
                     }
                 }
